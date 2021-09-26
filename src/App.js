d23b12d5f5832aca6979a81fce0bbb860596f296
davidtsui <davidtsuiart@gmail.com>
2021-09-26 22:10:12 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React from 'react'
+import './style.scss'
+import Calculator from './assets/calculator'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: 'style-a',
+      value: 1
+    }
+  }
+
+  handleToggle(key, value) {
+    this.setState({ [key]: value })
+    if (value == 1) {
+      this.setState({ theme: 'style-a' })
+    } else if (value == 2) {
+      this.setState({ theme: 'style-b' })
+    } else if (value == 3) {
+      this.setState({ theme: 'style-c' })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.theme} >
+        <div className="container">
+          <div className="navbar">
+            <div className="apptitle">calc</div>
+
+            <div className="nav-toggle">
+              <div className="theme">THEME</div>
+              <div className="theme-num">
+                <div className="themeNumber">1</div>
+                <div className="themeNumber">2</div>
+                <div className="themeNumber">3</div>
+              </div>
+              <div className="toggle-radio">
+                <input type='range' min='1' max='3' step='1'
+                  value={this.state.value}
+                  onChange={(e) => this.handleToggle('value', e.currentTarget.value)}
+                />
+              </div>
+            </div>
+
+          </div>
+          <Calculator />
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/assets/calculator.js
@@ -0,0 +1,112 @@
+import React from 'react'
+
+class Calculator extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      numpad_array: [
+        //set additional value attribute for different render and calculating value
+        { name: 7, value: 7 },
+        { name: 8, value: 8 },
+        { name: 9, value: 9 },
+        { name: 'DEL', value: 'DEL' },
+        { name: 4, value: 4 },
+        { name: 5, value: 5 },
+        { name: 6, value: 6 },
+        { name: '+', value: '+' },
+        { name: 1, value: 1 },
+        { name: 2, value: 2 },
+        { name: 3, value: 3 },
+        { name: '-', value: '-' },
+        { name: '.', value: '.' },
+        { name: 0, value: 0 },
+        { name: '/', value: '/' },
+        { name: 'x', value: 'x' },
+        { name: 'RESET', value: 'RESET' },
+        { name: 'equal', value: '=' }
+      ],
+      display: [],
+      calculate: []
+
+    }
+
+    this.handlechange = this.handlechange.bind(this);
+  }
+
+
+  handlechange(event) {
+    event.preventDefault()
+    const id = event.target.id;
+    const num = this.state.display
+
+    if (id === 'DEL') {
+      const split = num.toString()
+      this.setState({
+        display: split.slice(0, -1)
+      })
+      this.state.calculate.pop()
+    }
+
+    else if (id === 'equal') {
+      //using eval to directly calculate the string and outputs integers
+      const result = eval(this.state.calculate.join(''))
+      const resultdisplay = Math.round(result * 100) / 100
+      
+
+      this.setState({
+        //rounding up the result to 2 decimal points
+        display: resultdisplay,
+        calculate: resultdisplay.toString().split('')
+      })
+
+      console.log("display: " + this.state.display, "calculate: "+ this.state.calculate)
+    }
+    else if (id === 'x') {
+      this.setState({
+        display: num + '*'
+      })
+      this.state.calculate.push('*')
+    }
+
+    else if (id === 'RESET') {
+      this.setState({
+        display: [],
+        calculate: []
+      })
+    }
+
+    else {
+      this.setState({
+        display: num + `${id}`
+      })
+      this.state.calculate.push(id)
+    }
+
+
+  }
+
+  render() {
+    return (
+      <div className="calculator-display">
+        <div className="number-display"><div className="numdiv">{this.state.display}</div></div>
+        <div className="numpad">
+          <div className="numpad-container">
+            <ul className="row">
+              {this.state.numpad_array.map(item => {
+                return <li key={item.name.toString()}>
+                  <button id={item.name.toString()} onClick={this.handlechange}>{item.value}</button>
+                </li>
+              })}
+            </ul>
+
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default Calculator
